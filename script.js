edfca1466d4f650fca303458a765cd33065cddb8
GeorgeSwift16 <George.Swift@nology.io>
2022-11-23 15:48:51 +0000

    page content sorted, now to work out why it wont load

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {pokemonArray} from "./data/pokemon.js";
+import { pokemonArray } from "./data/pokemon.js";
 // data format is {id:1, name: "bulbasaur", types: ["grass", "poison"], sprite: ""}
 // we have an array within our object so will need element.type[0] and element.type[1]
 // the data is an array of objects - foreach
@@ -10,8 +10,8 @@ import {pokemonArray} from "./data/pokemon.js";
 // done. import the main container, make variable, make dom listener "load"
 // done. We need to set the listener to trigger our foreach function
 // 3. use innerhtml on maincontent to change this property
-// 4. Need to tell the foreeach function to change the innerhtml of card-container 
-// 5. need to get the foreach setup to use this format for each card 
+// 4. Need to tell the foreeach function to change the innerhtml of card-container
+// 5. need to get the foreach setup to use this format for each card
 /* <div card class>
 <img card__image class src="insert sprite:" alt="An image showing the pokem "insert name">
 <div card__content class>
@@ -23,33 +23,27 @@ import {pokemonArray} from "./data/pokemon.js";
 //          lets code
 
 // importing cardcontainer
-const cardContainer = document.querySelector(".card-container");
-console.log(cardContainer);
+const cardContainer = document.querySelector("#card-container");
+const header = document.querySelector("#h1");
+const renderPokemonCards = () => {
+  let cardshtml = "";
+  console.log(cardshtml);
 
-const renderPokemonCards = (event) => {
-    let cardshtml = "";
-
-    pokemonArray.forEach((element) => {
-
-        cardshtml += 
-        `<div class="card">
-        <img class="card__image" src=${element.sprite} alt="An image showing the pokemon ${element.name}.">
-        <div class="card__content">
-        <h2 class="card__heading">${element.name} has anyhting rendered?</h2>
-        <p class="card__text">${element.name} (#${element.id}) is a        ${element.types}  ()()need to look at type[1](()) pokemon.</p>
-        </div>
-        </div>`;
-        return cardshtml;
-    });
-    cardContainer.innerHTML = cardshtml;
+  pokemonArray.forEach((element) => {
+    cardshtml += `<div class="card">
+    <img class="card__image" src=${element.sprite} alt="An image showing the pokemon ${element.name}.">
+    <div class="card__content">
+    <h2 class="card__heading">${element.name}</h2>
+    <p class="card__text">${element.name} (#${element.id}) is a        ${element.types[0]} ${element.types[1]} pokemon.
+    </div>
+    </div>`;
+  });
+  console.log(cardshtml);
+  return cardContainer.innerHTML = cardshtml;
 };
 
-
-
-
-
-
-
-
 // dom element for cardcontainer
-cardContainer.addEventListener(`load`, renderPokemonCards);
+
+
+cardContainer.addEventListener("load", renderPokemonCards);
+header.addEventListener("click", renderPokemonCards);
